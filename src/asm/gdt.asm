766c87f76a9398a2248d93a75a8c93a6a6e6eed2
tushar1977 <steve192g@gmail.com>
2025-03-31 14:04:50 +0530

    added idt and gdt asm

--- src/asm/gdt.asm
+++ src/asm/gdt.asm
@@ -1,20 +1,28 @@
 global gdt_flush
-
+global tss_flush
+global reload_CS
+global reloadSegments
 gdt_flush:
-    MOV eax, [esp+4]
-    LGDT [eax]
-
-    MOV eax, 0x10
-    MOV ds, ax
-    MOV es, ax
-    MOV gs, ax
-    MOV ss, ax
-    JMP 0x08:.flush
-.flush:
-    RET
-
+    cli
+    lgdt [rdi]
+    jmp reloadSegments
+    sti
+    ret
+reloadSegments:
+    push 0x08
+    lea rax, [rel reload_CS]  
+    push rax                
+    retfq     
+reload_CS:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ret
 global tss_flush
 tss_flush:
-    MOV ax, 0x2B
-    LTR ax
-    RET
+    mov ax, 0x28
+    ltr ax
+    ret
--- src/asm/idt.asm
+++ src/asm/idt.asm
@@ -1,36 +1,30 @@
-global idt_flush
-idt_flush:
-    MOV eax, [esp+4]
-    LIDT [eax]
-    STI
-    RET
-
 %macro ISR_NOERRCODE 1
     global isr%1
     isr%1:
-        CLI
-        PUSH LONG 0
-        PUSH LONG %1
-        JMP isr_common_stub
+        cli
+        push qword 0     
+        push qword %1   
+        jmp isr_common_stub
 %endmacro
 
 %macro ISR_ERRCODE 1
     global isr%1
     isr%1:
-        CLI
-        PUSH LONG %1
-        JMP isr_common_stub
+        cli
+        push qword %1   
+        jmp isr_common_stub
 %endmacro
 
 %macro IRQ 2
     global irq%1
     irq%1:
-        CLI
-        PUSH LONG 0
-        PUSH LONG %2
-        JMP irq_common_stub
+        cli
+        push qword 0     
+        push qword %2   
+        jmp irq_common_stub
 %endmacro
 
+; Define ISRs
 ISR_NOERRCODE 0
 ISR_NOERRCODE 1
 ISR_NOERRCODE 2
@@ -39,14 +33,13 @@ ISR_NOERRCODE 4
 ISR_NOERRCODE 5
 ISR_NOERRCODE 6
 ISR_NOERRCODE 7
-
-ISR_ERRCODE 8
+ISR_ERRCODE   8
 ISR_NOERRCODE 9 
-ISR_ERRCODE 10
-ISR_ERRCODE 11
-ISR_ERRCODE 12
-ISR_ERRCODE 13
-ISR_ERRCODE 14
+ISR_ERRCODE  10
+ISR_ERRCODE  11
+ISR_ERRCODE  12
+ISR_ERRCODE  13
+ISR_ERRCODE  14
 ISR_NOERRCODE 15
 ISR_NOERRCODE 16
 ISR_NOERRCODE 17
@@ -67,7 +60,8 @@ ISR_NOERRCODE 31
 ISR_NOERRCODE 128
 ISR_NOERRCODE 177
 
-IRQ 0, 32
+; Define IRQs
+IRQ   0,    32
 IRQ   1,    33
 IRQ   2,    34
 IRQ   3,    35
@@ -84,60 +78,51 @@ IRQ  13,    45
 IRQ  14,    46
 IRQ  15,    47
 
+global isr_common_stub
 extern isr_handler
-isr_common_stub:
-    pusha
-    mov eax,ds
-    PUSH eax
-    MOV eax, cr2
-    PUSH eax
 
-    MOV ax, 0x10
-    MOV ds, ax
-    MOV es, ax
-    MOV fs, ax
-    MOV gs, ax
-
-    PUSH esp
-    CALL isr_handler
-
-    ADD esp, 8
-    POP ebx
-    MOV ds, bx
-    MOV es, bx
-    MOV fs, bx
-    MOV gs, bx
-
-    POPA
-    ADD esp, 8
-    STI
-    IRET
-
+global irq_common_stub
 extern irq_handler
+
+%macro pushad 0
+    push rax      
+    push rcx
+    push rdx
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    push r11
+%endmacro
+
+%macro popad 0
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rsi
+    pop rdi
+    pop rdx      
+    pop rcx
+    pop rax
+%endmacro
+
+
+isr_common_stub:
+    pushad
+    cld
+    lea rdi, [rsp]
+    call isr_handler
+    popad
+    add rsp, 0x10
+    iretq
+
 irq_common_stub:
-    pusha
-    mov eax,ds
-    PUSH eax
-    MOV eax, cr2
-    PUSH eax
-
-    MOV ax, 0x10
-    MOV ds, ax
-    MOV es, ax
-    MOV fs, ax
-    MOV gs, ax
-
-    PUSH esp
-    CALL irq_handler
-
-    ADD esp, 8
-    POP ebx
-    MOV ds, bx
-    MOV es, bx
-    MOV fs, bx
-    MOV gs, bx
-
-    POPA
-    ADD esp, 8
-    STI
-    IRET
+    pushad
+    cld
+    lea rdi, [rsp]
+    call irq_handler
+    popad
+    add rsp, 0x10
+    iretq
