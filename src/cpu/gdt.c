d2e7521fcfe15ee6aa36de02c48631df76b6a00f
tushar1977 <steve192g@gmail.com>
2025-03-31 14:05:16 +0530

    gdt and idt

--- src/cpu/gdt.c
+++ src/cpu/gdt.c
@@ -1,57 +1,60 @@
 #include "../include/gdt.h"
-
-#include "../include/stdio.h"
+#include "../include/kernel.h"
 #include "../include/string.h"
-#include "../include/util.h"
 #include <stdint.h>
 
-extern void gdt_flush(uint32_t);
+extern void gdt_flush(uint64_t);
 extern void tss_flush();
 
-struct gdt_entry gdt_entries[6];
-struct gdt_ptr gdt_ptr;
-struct tss_entry tss_entry;
+struct gdt_entry_struct gdt_entries[7];
+struct gdt_tss_entry_struct gdt_tss_entry;
+struct gdt_ptr_struct gdt_ptr;
+struct tss_entry_struct tss;
 
 void initGdt() {
 
-  gdt_ptr.limit = (sizeof(struct gdt_entry) * 6) - 1;
+  setGdtGate(0, 0, 0, 0, 0);            // Null segment
+  setGdtGate(1, 0, 0xFFFFF, 0x9A, 0xA); // Kernel code segment
+  setGdtGate(2, 0, 0xFFFFF, 0x92, 0xC); // Kernel data segment
+  setGdtGate(3, 0, 0xFFFFF, 0xFA, 0xA); // User code segment
+  setGdtGate(4, 0, 0xFFFFF, 0xF2, 0xC); // User data segment
+  write_tss(5, (uint64_t)&tss, sizeof(struct tss_entry_struct) - 1, 0x89, 0x0);
 
-  gdt_ptr.base = (uintptr_t)&gdt_entries;
-  setGdtGate(0, 0, 0, 0, 0);
-  setGdtGate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // kernel code segment
-  setGdtGate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // kernel data segment
-  setGdtGate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // user code segment
-  setGdtGate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // user data segment
-  writeTSS(5, 0x10, 0x0);
+  kernel.gdtr.base = (uint64_t)&gdt_entries;
+  kernel.gdtr.limit = (sizeof(struct gdt_entry_struct) * 7) - 1;
 
-  gdt_flush((uint32_t)&gdt_ptr);
+  gdt_flush((uint64_t)&kernel.gdtr);
   tss_flush();
 }
 
-void setGdtGate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access,
+void write_tss(int num, uint64_t base, uint64_t limit, uint8_t access,
+               uint8_t gran) {
+  memset(&tss, 0, sizeof(struct tss_entry_struct));
+
+  tss.rsp0 = 0xFFFFFFFF80000000;
+  tss.iopb_offset = sizeof(struct tss_entry_struct);
+
+  gdt_tss_entry.limit_low = (limit & 0xFFFF);
+  gdt_tss_entry.base_low = (base & 0xFFFF);
+  gdt_tss_entry.base_middle = (base >> 16) & 0xFF;
+  gdt_tss_entry.base_high = (base >> 24) & 0xFF;
+  gdt_tss_entry.base_upper32 = (base >> 32);
+  gdt_tss_entry.access = access;
+  gdt_tss_entry.limit_high = (limit >> 16) & 0xF;
+  gdt_tss_entry.flags = gran & 0xF;
+  gdt_tss_entry.reserved = 0;
+
+  *((struct gdt_tss_entry_struct *)&gdt_entries[num]) = gdt_tss_entry;
+}
+void setGdtGate(int num, uint64_t base, uint64_t limit, uint8_t access,
                 uint8_t gran) {
+  gdt_entries[num].limit_low = (limit & 0xFFFF);
 
   gdt_entries[num].base_low = (base & 0xFFFF);
-  gdt_entries[num].base_mid = (base >> 16) & 0xFF;
+  gdt_entries[num].base_middle = (base >> 16) & 0xFF;
   gdt_entries[num].base_high = (base >> 24) & 0xFF;
 
-  gdt_entries[num].limit = (limit & 0xFFFF);
-  gdt_entries[num].flags = (limit >> 16) & 0x0F;
-  gdt_entries[num].flags |= (gran & 0xF0);
+  gdt_entries[num].flags = gran;
 
   gdt_entries[num].access = access;
-}
-
-void writeTSS(uint32_t num, uint16_t ss0, uint32_t esp0) {
-  uint32_t base = (uint32_t)&tss_entry;
-  uint32_t limit = base + sizeof(tss_entry);
-
-  setGdtGate(num, base, limit, 0xE9, 0x00);
-  memset(&tss_entry, 0, sizeof(tss_entry));
-  tss_entry.ss0 = ss0;
-  tss_entry.esp0 = esp0;
-
-  tss_entry.cs = 0x08 | 0x3;
-  tss_entry.ss = tss_entry.ds = tss_entry.es = tss_entry.fs = tss_entry.gs =
-      0x10 | 0x3;
 }
