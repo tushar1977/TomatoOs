4622db4e5ba17e0509f2f104dd43ece471aaf8b0
tushar1977 <steve192g@gmail.com>
2025-04-01 21:38:25 +0530

    implemented macro functions

--- src/cpu/util.c
+++ src/cpu/util.c
@@ -11,3 +11,19 @@ char inPortB(uint16_t port) {
   asm volatile("inb %1, %0" : "=a"(rv) : "dN"(port));
   return rv;
 }
+
+void disable_interrupts() { asm("cli"); }
+
+void enable_interrupts() { asm("sti"); }
+
+void wait_for_interrupt() { asm("hlt"); }
+
+void halt() {
+  disable_interrupts();
+  for (;;)
+    wait_for_interrupt();
+}
+
+void cpuid(int code, uint32_t *a, uint32_t *d) {
+  asm volatile("cpuid" : "=a"(*a), "=d"(*d) : "0"(code) : "ebx", "ecx");
+}
